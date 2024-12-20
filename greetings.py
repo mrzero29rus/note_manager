0af65a12f2556102114892034815a4b0f1418e81
Artem <mrzero29@gmail.com>
2024-12-20 20:40:10 +0300

    20.12.24

---
+++ greetings.py
@@ -0,0 +1,13 @@
+username = "Артем"
+title = "Моя заметка"
+content = "Список дел"
+status = "Не выполнено"
+created_date = "18.12.24"
+issue_date = "20.12.24"
+
+print("Имя пользователя:", username)
+print("Загловок:", title)
+print("Содержание:", content)
+print("Статус:", status)
+print("Дата созданиия:", created_date)
+print("Дата выполнения:", issue_date)
