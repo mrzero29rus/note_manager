9b8535fada0455320fa81b646596233df4ea5f4c
Artem <mrzero29@gmail.com>
2024-12-25 20:38:46 +0300

    25.12.24

--- final.py
+++ final.py
@@ -5,11 +5,10 @@ content = input("Введите содержание заметки: ")
 created_date = input("Введите дату создания: ")
 issue_date = input("Введите дату выполнения: ")
 
-note = [
-    "Имя пользователя: ", username,
-    "Заголовки: ", [title1, title2],
-    "Содержание: ", content,
-    "Дата создания: ", created_date[:5],
-    "Дата выполнения: ", issue_date[:5]
-]
-print(note)
+note = [username, [title1, title2], content, created_date[:5], issue_date[:5]]
+
+print("Имя пользовател: ", note[0])
+print("Заголовки: ", note[1])
+print("Содержание: ", note[2])
+print("Дата создания: ", note[3])
+print("Дата выполнения: ", note[4])
