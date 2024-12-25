02a63f909d89d045e61daad226987141e2c70c73
Artem <mrzero29@gmail.com>
2024-12-25 20:40:47 +0300

    25.12.24

--- add_list.py
+++ add_list.py
@@ -1,5 +1,6 @@
 title1 = input("Заголовок 1: ")
 title2 = input("Заголовок 2: ")
 title3 = input("Заголовок 3: ")
-list = [title1, title2, title3]
-print(list)
+
+titles = [title1, title2, title3]
+print(titles)
