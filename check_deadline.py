7bcd461a74366472d241af9b615a518a5f25201f
Artem <mrzero29@gmail.com>
2025-01-03 23:17:54 +0300

    03.01.25

--- check_deadline.py
+++ check_deadline.py
@@ -1,3 +1,15 @@
+import datetime
+
+# current_date = datetime.date.today()
+# print(datetime.datetime.strftime(current_date, "%d-%m-%y"))
+
+# issue_date = input("Введите дату: ")
+# print(issue_date)
+
 from datetime import date
-current_date = date.today()
-print(current_date)
+
+year = int(input('Enter a year: '))
+month = int(input('Enter a month: '))
+day = int(input('Enter a day: '))
+d = date(day-month-year)
+print(d)
