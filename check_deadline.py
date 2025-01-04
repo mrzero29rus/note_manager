6dbb266ecb964df35df945803e134feed6d6d450
Artem <mrzero29@gmail.com>
2025-01-04 15:11:32 +0300

    04.01.25

--- check_deadline.py
+++ check_deadline.py
@@ -1,4 +1,23 @@
-import datetime
+from datetime import date, timedelta
 
-current_date = datetime.date.today()
-print(datetime.datetime.strftime(current_date, "%d-%m-%y"))
+current_date = date.today()
+print("Дата создания:", date.strftime(current_date, "%d-%m-%y"))
+
+print("\nВведите дату выполнения")
+year = int(input("Введите год: "))
+month = int(input("Введите месяц: "))
+day = int(input("Введите день: "))
+issue_date = date(year, month, day)
+
+print("\nДата создания:", date.strftime(current_date, "%d-%m-%y"))
+print("Дата выполнения:", date.strftime(issue_date, "%d-%m-%y"))
+
+check_deadline: timedelta = issue_date - current_date
+cd = check_deadline.days
+
+if cd > 1:
+    print("\nДо даты выполнения:", cd, "дней")
+elif cd == 0:
+    print("\nВнимание! Дата выполнения сегодня!")
+else:
+    print("\nВнимание! Дата выполнения истекла", cd, "дней назад")
