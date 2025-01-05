a5e9b4043fe61afac589303da3507a29de32c5e0
Artem <mrzero29@gmail.com>
2025-01-05 21:48:08 +0300

    05.01.25

---
+++ training.py
@@ -0,0 +1,14 @@
+from datetime import datetime
+
+current_date = datetime.today()
+print("Текущая дата: ", datetime.strftime(current_date, "%d-%m-%Y"))
+
+
+input_date = str(input("Введите дату (дд-мм-гггг): "))
+issue_date = datetime.strptime(input_date, "%d-%m-%Y")
+
+
+print("Текущая дата: ", datetime.strftime(current_date, "%d-%m-%Y"))
+print("Дата выполнения: ", datetime.strftime(issue_date, "%d-%m-%Y"))
+
+
