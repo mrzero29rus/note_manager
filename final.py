ff931beffb567a2840fa609091b8c4caaaf1569f
Artem <mrzero29@gmail.com>
2025-01-08 15:32:27 +0300

    07.01.25

--- final.py
+++ final.py
@@ -1,26 +1,24 @@
-# username = input("Введите ваше имя: ")
-# title1 = input("Введите название заметки: ")
-# title2 = input("Введите название заметки: ")
-# title3 = input("Введите название заметки: ")
-# content = input("Введите содержание заметки: ")
-# created_date = input("Введите дату создания: ")
-# issue_date = input("Введите дату выполнения: ")
-#
-# note = [username, [title1, title2, title3], content, created_date[:5], issue_date[:5]]
-#
-# print("Имя пользовател: ", note[0])
-# print("Заголовки: ", note[1])
-# print("Содержание: ", note[2])
-# print("Дата создания: ", note[3])
-# print("Дата выполнения: ", note[4])
+# создание списка для хранения информации о заметке
+# заголовки добавлены в отдельный вложенный список внутри основного списка
+
+username = input("Введите ваше имя: ")
+
+title1 = input("Заголовок 1: ")
+title2 = input("Заголовок 2: ")
+title3 = input("Заголовок 3: ")
+titles_list = [title1, title2, title3]
+
+titles = titles_list
+content = input("Введите содержание заметки: ")
+created_date = input("Введите дату создания (дд-мм-гггг): ")
+issue_date = input("Введите дату выполнения (дд-мм-гггг): ")
 
 note = [
-    input("Имя пользователя: "),
-    input("Введите заголовок: "),
-    input("Введите содержание: "),
-    input("Статус заметки: "),
-    input("Дата создания: "),
-    input("Дата выполнения: ")
+    username,
+    titles,
+    content,
+    created_date,
+    issue_date,
 ]
 
 print(note)
--- greetings.py
+++ greetings.py
@@ -1,3 +1,5 @@
+# созданы базовые переменные для заметки
+
 username = "Артем"
 title = "Моя заметка"
 content = "Список дел"
@@ -5,16 +7,11 @@ status = "Не выполнено"
 created_date = "18.12.24"
 issue_date = "20.12.24"
 
+# вывод значений организован через print
+
 print("Имя пользователя:", username)
 print("Загловок:", title)
 print("Содержание:", content)
 print("Статус:", status)
 print("Дата созданиия:", created_date)
 print("Дата выполнения:", issue_date)
-
-# возможно заменить
-# from datetime import date
-# current_date = date.today()
-# issue_date = date(2025, 1, 31)
-# print(date.strftime(current_date, "%d-%m-%y"))
-# print(date.strftime(issue_date, "%d-%m-%y"))
