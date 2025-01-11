f7c2e7c2fb4ee6a79702e8decd858c62d4ea8330
Artem <mrzero29@gmail.com>
2025-01-11 20:48:11 +0300

    11.01.25 titles loop, update status, check deadline, rename files

--- 1.2.3 check_deadline.py
+++ 1.2.3 check_deadline.py
@@ -4,27 +4,32 @@
 # Функция для расчёта разницы между датами
 # Функция для обработки пользовательского ввода
 
-
 from datetime import datetime, timedelta
 
-# получение текущей даты, конвертация в формат дд-мм-гггг и вывод на экран
-current_date = datetime.today()
-print("Текущая дата: ", datetime.strftime(current_date, "%d-%m-%Y"))
+# функция получения текущей даты и конвертация в формат дд-мм-гггг
+def current_date_function():
+    current_date = datetime.strftime(datetime.today(), "%d-%m-%Y")
+    return current_date
 
 # запрос от пользователя ввода даты выполнения
-input_date = str(input("Введите дату выполнения (дд-мм-гггг): "))
-issue_date = datetime.strptime(input_date, "%d-%m-%Y")
-
-print("Текущая дата: ", datetime.strftime(current_date, "%d-%m-%Y"))
-print("Дата выполнения: ", datetime.strftime(issue_date, "%d-%m-%Y"))
+def input_issue_date_function():
+    issue_date = datetime.strptime(str(input("Введите дату выполнения (дд-мм-гггг): ")), "%d-%m-%Y")
+    print("Дата выполнения:", datetime.strftime(issue_date, "%d-%m-%Y"))
+    return issue_date
 
 # расчет дней на исполнение или количества дней просрочки
-delta = issue_date.day - current_date.day
+def delta_function():
+    delta = input_issue_date_function().day - datetime.today().day
+    print(delta)
+    # if timedelta > 1:
+    #     print("\nДо даты выполнения:", timedelta, "дней")
+    # elif timedelta == 0:
+    #     print("\nВнимание! Дата выполнения сегодня!")
+    # else:
+    #     print("\nВнимание! Дата выполнения истекла", timedelta, "дней назад")
 
-# вывод дней на исполнение или количества дней просрочки
-if delta > 1:
-    print("\nДо даты выполнения:", delta, "дней")
-elif delta == 0:
-    print("\nВнимание! Дата выполнения сегодня!")
-else:
-    print("\nВнимание! Дата выполнения истекла", delta, "дней назад")
+
+# print("Текущая дата: ", datetime.strftime(current_date, "%d-%m-%Y"))
+# print("Дата выполнения: ", datetime.strftime(issue_date, "%d-%m-%Y"))
+
+delta_function()
